c646b747cdde343a4d0135bf475734804cfeeaa1
Rafal Becla <rafal.becla@gmail.com>
2018-05-22 11:14:25 +0200

    Created main menu schema

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-# main functions
+import log_in
+import register
 
-#init dev try push 
+def main():
+    while True:
+        handle_menu()
+        try:
+            choose()
+        except KeyError as err:
+            ui.print_error_message(err)
+
+
+def choose():
+    'choses from options'
+    option = input("Please choose position from menu: ")
+    if option == "1":
+        # log in function
+    elif option == "2":
+        # register function
+    elif option == "0":
+        sys.exit(0)
+    else:
+        raise KeyError("There is no such option.")
+
+
+if __name__ == '__main__':
+    main()

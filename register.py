051453c7da2e14065078c9893e9c50337402ec48
darDomanski <dardomanski@gmail.com>
2018-05-22 15:41:04 +0200

    add methods in User() class

--- register.py
+++ register.py
@@ -4,9 +4,11 @@ class User():
         self.surname = input("Surname: ")
         self.password = self.create_password()
         self.gender = self.get_gender()
-        self.age = input("Age: ")
+        self.age = self.assign_int("age")
+        self.weight = self.assign_int("weight")
+        self.goals = self.choose_goals()
         self.file_name = "users/" + self.name.upper() + "_" + self.surname.upper() + ".txt"
-
+        
     def get_data_from_user(self, data_type):
         user_data = input(data_type)
         return user_data
@@ -20,16 +22,6 @@ class User():
     def get_password(self):
         return self.password
 
-    def find_user_data_file(self):
-        name = input("Your name: ")
-        surname = input("Your surname: ")
-        filename = "users/" + name.upper() + "_" + surname.upper() + ".txt"
-        try:
-            with open(filename, "r") as user_data_file:
-                return True
-        except:
-            return False
-
     def get_gender(self):
         gender_ok = None
         while gender_ok is None:
@@ -42,7 +34,7 @@ class User():
 
     def change_name(self, new_name):
         self.name = new_name
-
+            
     def create_password(self):
         password_ok = None
         while password_ok is None:
@@ -55,24 +47,46 @@ class User():
             else:
                 print("Password is not correct, try again.")
 
+    def choose_goals(self):
+        goals = {1: 'lose body fat', 2: 'hypertrophy', 3: 'strength'}
+        print("Choose your goal: ", goals)
+
+        goal_choosen = False
+        while goal_choosen is False:
+            choose = int(input("Type number of goal: "))
+            if choose in (1, 2, 3):
+                goal_choosen = True
+                return goals[choose]
+            else:
+                print("Unknown choice, try again.")
+            
+    def assign_int(self, value_name):
+        is_int = False
+        while is_int is False:
+            number = input("Type your " + value_name + ": ")
+            if number.isdigit():
+                return number
+                is_int = True
+            else:
+                print("It's not a number!")
+
     def user_data_to_export(self):
         return "\t".join(string for string in [self.name,
                                                self.surname,
                                                self.password,
                                                self.gender,
-                                               self.age])
+                                               str(self.age),
+                                               str(self.weight),
+                                               self.goals])
 
     def export_data(self):
         with open(self.file_name, "w") as data_file:
             data_file.write(self.user_data_to_export())
 
 
-def main():
-    dupa = User()
-    print(dupa.file_name)
-    print(dupa.user_data_to_export())
-    print(dupa.find_user_data_file())
-    dupa.export_data()
+def start_module():
+    user = User()
+    user.export_data()
 
 
-main()
+start_module()

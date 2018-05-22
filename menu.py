42c09a659919c28d6e303aac408f87f14884bc9a
Rafal Becla <rafal.becla@gmail.com>
2018-05-22 11:03:56 +0200

    Created schema of menu

---
+++ menu.py
@@ -0,0 +1,26 @@
+def start_module():
+    option = 1
+    while option != '0':
+        print_menu()
+        option = input('Please choose option')
+        if option == "1":
+            # 
+        elif option == "2":
+            #            
+        elif option == "3":
+            #             
+        elif option == "4":
+            #             
+        elif option == "5":
+            # 
+        elif option == "6":
+            #
+        elif option == "0":
+            print('Thanks for using our training programm! Good luck!')
+            # funkcja do zapisania danych?
+        else:
+            raise KeyError("There is no such option.")
+
+
+def print_menu():
+    pass

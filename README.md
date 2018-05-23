ec77f20bde18ef8e9c8e5e237ea3c0a1c54135fb
Michal Wisniewski <mihuwis@gmail.com>
2018-05-23 15:35:54 +0200

    created export to txt

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# Training_Logbook
+# Training Log
+
+Test your abilities. Get one of our unique workout programs and log your progress!!!
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+Give examples
+```
+
+### Installing
+
+A step by step series of examples that tell you have to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Built With
+
+* 
+
+## Contributing
+
+
+## Versioning
+
+
+## Authors
+* **Rafał Becla** - *Code dev, System architecture* - 
+* **Michal Wisniewski** - *Concept development, Code dev* - [mihuwis](https://github.com/mihuwis)
+* **Dariusz Domanski** - *Code dev* - 
+
+## Acknowledgments
+
+* all the mentors of Codecool - [codecool](https://codecool.pl/?gclid=CjwKCAjw_47YBRBxEiwAYuKdw5QfaAtMXb6cJ8797CknglulW_YCtPZFojwZF1szWyBIPaai0eeQBxoClMEQAvD_BwE)
+* 
--- log.py
+++ log.py
@@ -1,6 +1,7 @@
 # log book - open it when on the gym. PrintOut exercises and get results.
 
 import time
+# import menu
 
 
 class LogBook:
@@ -8,8 +9,6 @@ class LogBook:
     def __init__(self, workout_for_today,):
         self.workout_for_today = workout_for_today
         self.exercises_list = self.load_file()
-
-        # self.set =
 
     def load_file(self):
 
@@ -26,28 +25,57 @@ class LogBook:
     def print_workout(self):
         print("\nHello! Today you have following exercises to do:\n")
         for i in range(len(self.exercises_list)):           
-            print('\tExercise no{}. {}. Do {} sets of {} repetitions. Rest {}sek betweene sets. Your tempo should be {}'
-                  .format(i+1, self.load_file()[i][1], self.load_file()[i][2], self.load_file()[i][3],
-                          self.load_file()[i][4], self.load_file()[i][6]))
+            print('Exercise no{0}\n\t{1}. Do {2} sets of {3} repetitions with {5}kg\n\tRest {4}sek between sets.'
+                  'Your tempo should be {6}\n'
+                  .format(i+1, self.exercises_list[i][1], self.exercises_list[i][2], self.exercises_list[i][3],
+                          self.exercises_list[i][4], self.exercises_list[i][5], self.exercises_list[i][6]))
+        self.start_exercise()
 
     def start_exercise(self):
-
-        try:
-            lest_start = input("")
-        for i in range(len(self.exercises_list)):
-            print("Do excersise no{}\n\t-{}".format(i+1, self.load_file()[i][1]))
+        corect_input = False
+        while not corect_input:
+            try:
+                lets_start = str.upper(input("Insert Y to start or N to quit to menu: "))
+                if lets_start == 'Y':
+                    self.get_workout_results()
+                    corect_input = True
+                    # self.workout_duration()
+                elif lets_start == 'N':
+                    print("stop")
+                    exit()
+                    # menu.start_module()
+                else:
+                    raise ValueError('{} is not corect choice.'.format(lets_start))
+            except ValueError as err:
+                print('You entered wrong input! {}'.format(err))
 
     def get_workout_results(self):
-        pass
+
+        for an_excercise in range(len(self.exercises_list)):
+            print("Do excersise no{}\n\t-{}".format(an_excercise+1, self.load_file()[an_excercise][1]))
+            no_of_sets = int(self.exercises_list[an_excercise][2])
+            self.log_result_in(self.exercises_list[an_excercise][1])
+            for a_set in range(no_of_sets):
+                corect_input = False
+                while not corect_input:
+                    try:
+                        result_of_set = int(input('Set no{}. Enter here no of reps: '.format(a_set + 1)))
+                        self.log_result_in(result_of_set)
+                    except ValueError:
+                        print("Please enter number of reps!")
+                    else:
+                        corect_input = True
+            self.log_result_in('\n')
+
+    def log_result_in(self, an_input):
+        with open('workoout_user_date', 'a') as file:
+            file.write(str(an_input) + ' ')
 
 
 def start_module():
-    cwiczymy = LogBook('exercises.txt')
+    cwiczymy = LogBook('my_fbw.txt')
 
     cwiczymy.print_workout()
-    cwiczymy.start_exercise()
 
 
 start_module()
-
-
